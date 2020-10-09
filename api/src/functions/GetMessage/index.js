c0b943fe025bd5400825bef65f4d4172ae1870f7
Johan Eliasson <jeliasson@users.noreply.github.com>
2020-10-09 15:29:27 +0200

    Upgrading RedwoodJS to 0.19.2 (#1)

--- api/src/functions/GetMessage/index.js
+++ api/src/functions/GetMessage/index.js
@@ -1,7 +1,8 @@
-module.exports = async function (context, req) {
-    context.res = {
+  export const handler = async (event, context) => {
+    return {
+      statusCode: 200,
       body: {
         text: "Hello from the API"
       }
-    };
-  };
+    }
+  }
